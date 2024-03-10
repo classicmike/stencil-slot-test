c18879a25a9af8ebfeee5af98f59084a375de129
michaeltran <michael.tran@sandstone.com.au>
2024-03-10 14:28:26 +1100

    Added Dist

---
+++ packages/react-library/dist/components/stencil-generated/index.js
@@ -0,0 +1,5 @@
+import { createReactComponent } from './react-component-lib';
+export const MyComponent = createReactComponent('my-component');
+export const TestBtn = createReactComponent('test-btn');
+export const TestContainer = createReactComponent('test-container');
+//# sourceMappingURL=index.js.map
