55e3bb3b87a4a9498918b48e8696fb0c340e8266
michaeltran <michael.tran@sandstone.com.au>
2024-03-10 14:36:03 +1100

    Removing Dist

--- packages/react-library/dist/components/stencil-generated/index.js
+++
@@ -1,5 +0,0 @@
-import { createReactComponent } from './react-component-lib';
-export const MyComponent = createReactComponent('my-component');
-export const TestBtn = createReactComponent('test-btn');
-export const TestContainer = createReactComponent('test-container');
-//# sourceMappingURL=index.js.map
--- packages/react-library/dist/types/components/stencil-generated/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-import type { JSX } from 'stencil-library';
-export declare const MyComponent: import("react").ForwardRefExoticComponent<JSX.MyComponent & Omit<import("react").HTMLAttributes<HTMLMyComponentElement>, "style"> & import("./react-component-lib/interfaces").StyleReactProps & import("react").RefAttributes<HTMLMyComponentElement>>;
-export declare const TestBtn: import("react").ForwardRefExoticComponent<JSX.TestBtn & Omit<import("react").HTMLAttributes<HTMLTestBtnElement>, "style"> & import("./react-component-lib/interfaces").StyleReactProps & import("react").RefAttributes<HTMLTestBtnElement>>;
-export declare const TestContainer: import("react").ForwardRefExoticComponent<JSX.TestContainer & Omit<import("react").HTMLAttributes<HTMLTestContainerElement>, "style"> & import("./react-component-lib/interfaces").StyleReactProps & import("react").RefAttributes<HTMLTestContainerElement>>;
